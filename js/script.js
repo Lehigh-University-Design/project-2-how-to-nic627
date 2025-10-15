e25fb6d9ca2d830dc9b7f3d8cf87b0ac042272f5
nic627 <157721410+nic627@users.noreply.github.com>
2025-10-15 06:11:30 -0400

    project done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // Start JS script code, feel free to not use this or remove it
+// Enable smooth scroll behavior
+document.querySelectorAll('.navbar a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
 
-console.log("Hello, World!");
